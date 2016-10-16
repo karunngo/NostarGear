5e85ce365d201aff1884e8e3c93f51906543d931
Ayami Orihara <karu@life-cloud.ht.sfc.keio.ac.jp>
2016-10-16 20:53:38 +0900

    tableの名前を修正

--- test_NostalGear.php
+++ test_NostalGear.php
@@ -24,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
 
         	//sqlでパス取得
-		$sql = "SELECT path from remember ORDER BY date DESC LIMIT 1 WHERE name = " . $thing_name;
+		$sql = "SELECT path from NostalGear ORDER BY date DESC LIMIT 1 WHERE name = " . $thing_name;
         	$mysql->query($sql);
 		$sql_result = $mysql->fetch();
 
