22babaf81a936d0026de105763bb5c371e363417
Ayami Orihara <karu@life-cloud.ht.sfc.keio.ac.jp>
2016-10-16 20:51:38 +0900

    一通り完成

---
+++ cloud_vision.php
@@ -0,0 +1,47 @@
+<?php
+class Cloud_vison{
+	$api_key = "AIzaSyBsmdrzulnPMj2B9Zf9yEmy3e6kEgqgkPM" ;
+
+	function get_label($path){
+		
+		$image_path = $path;
+		//$image_path = "http://life-cloud.ht.sfc.keio.ac.jp/~karu/orf/image/toriaezu.jpg" ;
+		// リクエスト用のJSONを作成
+        	$json = json_encode( array(
+        	        "requests" => array(
+        	                array(
+        	                        "image" => array(
+        	                                "content" => base64_encode( file_get_contents( $image_path ) ) ,
+        	                        ) ,
+       		                         "features" => array(
+        	                                array(
+        	                                        "type" => "LABEL_DETECTION" ,
+        	                                        "maxResults" => 1 ,
+        	                                ) ,
+        	                        ) ,
+        	                ) ,
+        	        ) ,
+        	) ) ;
+
+	        // リクエストを実行
+	        $curl = curl_init() ;
+	        curl_setopt( $curl, CURLOPT_URL, "https://vision.googleapis.com/v1/images:annotate?key=" . $api_key ) ;
+	        curl_setopt( $curl, CURLOPT_HEADER, true ) ;
+	        curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "POST" ) ;
+	        curl_setopt( $curl, CURLOPT_HTTPHEADER, array( "Content-Type: application/json" ) ) ;
+	        curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false ) ;
+	        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true ) ;
+	        if( isset($referer) && !empty($referer) ) curl_setopt( $curl, CURLOPT_REFERER, $referer ) ;
+	        curl_setopt( $curl, CURLOPT_TIMEOUT, 15 ) ;
+	        curl_setopt( $curl, CURLOPT_POSTFIELDS, $json ) ;
+	        $res1 = curl_exec( $curl ) ;
+	        $res2 = curl_getinfo( $curl ) ;
+	        curl_close( $curl ) ;
+	
+	        // 取得したデータ
+	        $json = substr( $res1, $res2["header_size"] ) ;                         // 取得したJSON
+	        $header = substr( $res1, 0, $res2["header_size"] ) ;            // レスポンスヘッダー
+		return $json;
+	}
+}
+
---
+++ test_NostalGear.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("mysql.php");
+require_once("cloud_vision.php");
+
+$mysql =new MySQL;
+$cloud_vison = new Cloud_vision;
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+        if ($_POST["type"]=="vision"){
+		$image = $_FILES["file"]["name"];
+
+		//一旦保存する
+		$save_url = "";		
+		move_uploaded_file($_FILES["file"]["tmp_name"], $save_url);
+	
+		//名前をgetする
+		$label_arr = json_decode($cloud_vision->get_label($save_url),true);
+
+		echo $label_arr;
+
+		$thing_name = $label_arr[`responses`][`labelAnnotations`][0][`description`]; 		
+
+
+        	//sqlでパス取得
+		$sql = "SELECT path from remember ORDER BY date DESC LIMIT 1 WHERE name = " . $thing_name;
+        	$mysql->query($sql);
+		$sql_result = $mysql->fetch();
+
+		echo $sql_result;
+	
+		if($mysql->rows() == 0){
+			//echo "error! " . $things_name ."の思い出はありません";
+		}else{
+			//$vision_path = "http://life-cloud.ht.sfc.keio.ac.jp/~karu/orf/image/" . $$sql_result[0][`path`];
+			//return $vision_path
+		}
+	}
+
+
+}else{
+echo "error! post以外の通信です"";
+}
+?>
+
